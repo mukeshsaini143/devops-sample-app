5de9af7e0170c7d2731c98d38b0c0b91d1597a0a
mukeshsaini143 <94359081+mukeshsaini143@users.noreply.github.com>
2025-03-22 15:49:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{message || 'Loading...'}</h1>
+        <h1>{message || 'hello world'}</h1>
       </header>
     </div>
   );
